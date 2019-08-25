fac46b06e9555aed4111ad7bdeb1adc43795a16c
Unknown <harisk@student.unimelb.edu.au>
2019-08-25 11:52:32 +1000

    Added Swagger V2

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -1,20 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+
 
 namespace WebAPI
 {
     public class Startup
     {
+        private const string swaggerVersion = "v2";
+        private const string swaggerTitle = "CarSales MVP";
+        private const string swaggerDocName = "v2";
+        
+
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -26,6 +26,14 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                
+                c.SwaggerDoc(swaggerDocName, new Swashbuckle.AspNetCore.Swagger.Info { Title = swaggerTitle, Version = swaggerVersion });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +47,19 @@ namespace WebAPI
             {
                 app.UseHsts();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            string swaggerEndPoint = $"../swagger/{swaggerDocName}/swagger.json";
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+
+                c.SwaggerEndpoint(swaggerEndPoint, swaggerTitle);
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
             app.UseMvc();
